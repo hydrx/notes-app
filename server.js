3c09eea95be793239424061ae2c33fff9a39f4d0
Ticia Francisco <hydracodone@gmail.com>
2022-09-23 11:30:37 -0700

    Update server.js and packages

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const methodOverride = require("method-override");
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
+const storyRoutes = require('./routes/stories')
+const entryRoutes = require('./routes/entries')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -22,6 +24,9 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
 
 // Sessions
 app.use(
@@ -41,6 +46,7 @@ app.use(flash())
 
 app.use('/', mainRoutes)
 app.use('/stories', storyRoutes)
+app.use('/entries', entryRoutes)
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
