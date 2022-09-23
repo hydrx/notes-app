0a52491a04100023c3bd7142c2e8210abfd0ea37
Ticia Francisco <hydracodone@gmail.com>
2022-09-23 11:24:54 -0700

    Add models

---
+++ models/Story.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const StorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Story", StorySchema);
