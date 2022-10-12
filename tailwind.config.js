055aeb2af8f81364ac12646766ebe04ceed155e4
Ticia Francisco <hydracodone@gmail.com>
2022-10-12 12:19:51 -0700

    Add initial tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],,
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "synthwave","pastel","night"],
+  },
 }
